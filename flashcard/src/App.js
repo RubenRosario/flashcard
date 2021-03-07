5ab4f10e7145a84beadf9aaaee5aea6ff66157ca
RubenRosario_1101068 <1101068@isep.ipp.pt>
2021-03-07 14:17:55 -0500

    initial commit

---
+++ flashcard/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="app-component">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
