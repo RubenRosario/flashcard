e05a108bd4115ff0db904169d8be9244651e4a30
RubenRosario_1101068 <1101068@isep.ipp.pt>
2021-03-15 23:09:40 -0400

    add word and definitions (not styled yet)

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './Card';
 
+const wordsList = [
+  {
+    word: 'overwhelm',
+    definition: {
+      verb: ['To defeat completely.', 'To inundate.', 'To overpower.'],
+      noun: ['overwhelming'],
+      usage: [],
+      translation: { portuguese: [] },
+    },
+  },
+];
+
 function App() {
+  const random = Math.random();
   return (
     <div className="app-component">
-      <Card />
+      <Card front={wordsList[0].word} back={wordsList[0].definition} />
     </div>
   );
 }
--- flashcard/src/Card.js
+++ flashcard/src/Card.js
@@ -7,11 +7,26 @@ const flip = () => {
 };
 
 const Card = (props) => {
+  const word = props.front;
+  const { verb, noun, usage, translation } = props.back;
+  console.log(`${verb} ${noun} ${usage} ${translation}`);
   return (
     <div className="scene scene--card">
       <div className="card" onClick={flip}>
-        <div className="card__face card__face--front">front</div>
-        <div className="card__face card__face--back">back</div>
+        <div className="card__face card__face--front">{word}</div>
+        <div className="card__face card__face--back">
+          <ul>
+            verb:{' '}
+            {verb.map((w) => (
+              <li>{w}</li>
+            ))}
+            {/* <li>{`noun: ${noun.foreach((w) => <span>{w}</span>)}`}</li>
+            {usage && (
+              <li>{`usage: ${usage.foreach((w) => <span>{w}</span>)}`}</li>
+            )}
+            <li>{`translation: ${translation}`}</li> */}
+          </ul>
+        </div>
       </div>
     </div>
   );
