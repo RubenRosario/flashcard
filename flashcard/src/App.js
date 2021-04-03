ab813d6818f5a472782073b0f90b20202e36a09d
RubenRosario_1101068 <1101068@isep.ipp.pt>
2021-04-03 00:09:08 -0400

    add more words

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -4,12 +4,83 @@ import Card from './Card';
 
 const wordsList = [
   {
-    word: 'overwhelm',
+    word: 'Overwhelm',
     definition: {
-      verb: ['To defeat completely.', 'To inundate.', 'To overpower.'],
-      noun: ['overwhelming'],
+      is: ['Verb'],
+      meaning: [
+        'To defeat someone of something using a lot of force.',
+        'To cause someone to feed sudden strong emotion.',
+        'To be to much to deal with.',
+      ],
+      synonymous: [
+        'overpower',
+        'overcome',
+        'inundate',
+        'overload',
+        'overburden',
+      ],
+      usage: [
+        'the troops have overwhelmed the rebels, and seized control of the capital',
+        'I am overwhelmed by so many birthday wishes',
+        'I am overwhelmed with too much work to do.',
+      ],
+      translation: { portuguese: ['derrotar', 'inundar', 'sobrecarregar'] },
+    },
+  },
+  {
+    word: 'Opinionated',
+    definition: {
+      is: ['Adjective'],
+      meaning: [
+        'An opinionated person is certain about their beliefs, and expresses their ideas strongly and often',
+      ],
+      synonymous: ['inflexible', 'arrogant', 'assertive', 'dogmatic'],
       usage: [],
-      translation: { portuguese: [] },
+      translation: { portuguese: ['teimoso'] },
+    },
+  },
+  {
+    word: 'Esoteric',
+    definition: {
+      is: ['Adjective'],
+      meaning: [
+        'Intended for a small group of people',
+        'Require special knowledge or interest',
+        'Likely to be understood by a small group of people',
+      ],
+      synonymous: [
+        'obscure',
+        'abstract',
+        'complex',
+        'enigmatic',
+        'occult',
+        'secret',
+        'private',
+      ],
+      usage: [
+        'This documentary is too esoteric for my taste.',
+        'I enjoy reading esoteric books',
+      ],
+      translation: { portuguese: ['Esoterico'] },
+    },
+  },
+  {
+    word: 'Plunge',
+    definition: {
+      is: ['verb', 'noun'],
+      meaning: [
+        'to (cause someone or something to) move or fall suddenly and often a long way forward, down, or into something.',
+        'To jump or dive quickly',
+        'To push or thrust quickly',
+      ],
+      synonymous: ['Jump', 'Dive', 'Thrust', 'Push', 'Shove'],
+      usage: [
+        'I ran down the beach and plunged into the sea',
+        'he plunged his hands into his pockets',
+      ],
+      translation: {
+        portuguese: ['mergulhar', 'submerger', 'mergulho', 'imersao'],
+      },
     },
   },
 ];
